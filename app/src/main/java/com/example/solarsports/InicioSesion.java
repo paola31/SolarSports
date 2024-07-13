203d4e4e5f346edb6d2acb858ddda1a7728ed9e2
paola <paolisco2525@gmail.com>
2024-07-12 22:25:49 -0500

    add views

--- app/src/main/java/com/example/solarsports/InicioSesion.java
+++ app/src/main/java/com/example/solarsports/InicioSesion.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 
@@ -18,6 +19,7 @@ public class InicioSesion extends AppCompatActivity
 
         TextView textView = findViewById(R.id.olvidasteContraseña);
         textView.setOnClickListener(new View.OnClickListener()
+
         {
             public void onClick(View v)
             {
@@ -25,5 +27,28 @@ public class InicioSesion extends AppCompatActivity
                 startActivity(intent);
             }
         });
+
+        TextView textViewRegistrate = findViewById(R.id.registrate);
+        textViewRegistrate.setOnClickListener(new View.OnClickListener()
+
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(InicioSesion.this, Registro.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton imageButtonlogin = findViewById(R.id.login_Sesion);
+        imageButtonlogin.setOnClickListener(new View.OnClickListener()
+
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(InicioSesion.this, PantallaPpal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
--- app/src/main/java/com/example/solarsports/Registro.java
+++ app/src/main/java/com/example/solarsports/Registro.java
@@ -6,27 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
-public class RecuperarContrasena extends AppCompatActivity
+public class Registro extends AppCompatActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recuperar_contrasena);
-
-        ImageButton logoutButton = findViewById(R.id.logOut);
-        logoutButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(RecuperarContrasena.this, InicioSesion.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_registro);
 
         ImageButton loginButton = findViewById(R.id.login_Recuper);
         loginButton.setOnClickListener(new View.OnClickListener()
@@ -34,7 +22,18 @@ public class RecuperarContrasena extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(RecuperarContrasena.this, InicioSesion.class);
+                Intent intent = new Intent(Registro.this, InicioSesion.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton logoutButton = findViewById(R.id.logOutResgistro);
+        logoutButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Registro.this, InicioSesion.class);
                 startActivity(intent);
             }
         });
